e8a769dc625ab0d3ed796cf7e5e7fb46f62f7b91
enotarangelo <notarangelo275@gmail.com>
2021-10-31 10:47:11 +0100

    Mixed Message

---
+++ Mixed Message/Mixed_Message.js
@@ -0,0 +1,16 @@
+const message={
+    short= {
+        message:"Ciao come stai ?"
+    },
+    medium= {
+        message:"Ciao come va? io tutto bene"
+    },
+    long= {
+        message:"Ciao è un po' di tempo che non ci vediamo, come stai? io bene "
+    }
+}
+
+
+const random_message=obj=>{
+    
+}
